f3449af27882350000c1e29117adf088daa349e4
Jisook Moon <moonj.kr@gmail.com>
2017-02-12 16:19:41 -0800

    new changes

--- TeamCode/src/main/java/srFile/SR_teleop.java
+++ TeamCode/src/main/java/srFile/SR_teleop.java
@@ -1,9 +1,9 @@
 
-
-package org.firstinspires.ftc.teamcode;
+package srFile;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -11,23 +11,27 @@ import com.qualcomm.robotcore.util.Range;
 
 
 /**
- * Created by Jisook Moon on 2/10/17
- * Super Regionals
+ * Created by Jisook Moon on 1/5/17
+ * State Competition
  * /
- 
-/////////Instructions///////////////
+
+ /////////Instructions///////////////
+
  gamepad 1:
+
  Drive - Joysticks UP & DOWN
  Beacon Presser - right bumper is PUSH, right trigger is RETRACT
- 
+
  gamepad 2:
+
  Shooter - button 'b' is STOP 'a' is RUN
  Arm Release - button 'a' is RELEASE, 'b' is servo facing back up
  Lift - Controller 2: Joysticks UP & DOWN
- 
+
+
  //Notes
- - set initial value for shooter motor
- - 
+ - fix directions for motors
+ -
  */
 
 
@@ -38,27 +42,26 @@ public class SR_teleop extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
 
     // motor declarations
-    DcMotor     M_drive_L = null,
-                M_drive_R = null,
-                M_shooter = null,
-                M_pickup = null;
+    DcMotor M_drive_L = null,
+            M_drive_R = null,
+            M_pickup_TOP = null,
+            M_pickup_BOTTOM = null,
+            M_shooter = null;
 
     // servo declarations
-    Servo       S_button_L = null,
-                S_button_R = null,
-                S_ballDrop = null;
+    CRServo S_button_L;
+    CRServo S_button_R;
+    Servo S_ballDrop_TELEOP;
+    Servo S_ballDrop_AUTON;
 
     // all of the starting servo positions
-    final double INIT_BUTTON_POS = 0.5,
-                 BALL_DROP_INIT = 0.2;
-                 
-   // all of the starting motor positions
-   final double INIT_SHOOTER_POS = 0.0;
+    final double BUTTON_INIT_STOP = 0.5,
+                 BALL_DROP_AUTON_INIT = 0.2,
+                 BALL_DROP_TELEOP_INIT = 0.2;
 
-    double ARM_POS_L = ARM_INIT_POS_L,
-           ARM_POS_R = ARM_INIT_POS_R,
-           BUTTON_POS = BUTTON_INIT_POS,
-           BALL_DROP = BALL_DROP_INIT;
+    double  BUTTON_POS = BUTTON_INIT_STOP,
+            BALL_DROP_AUTON_POS = BALL_DROP_AUTON_INIT,
+            BALL_DROP_TELEOP_POS = BALL_DROP_TELEOP_INIT;
 
     // servo constant
     double SERVO_TICK = 0.03;
@@ -69,28 +72,24 @@ public class SR_teleop extends LinearOpMode {
         telemetry.update();
 
         // mapping motor variables to their hardware counterparts
-        this.M_drive_L = hardwareMap.dcMotor.get("M_drive_L");
-        this.M_drive_R = hardwareMap.dcMotor.get("M_drive_R");
-
+        this.M_drive_L = hardwareMap.dcMotor.get("M_drive_BL");
+        this.M_drive_R = hardwareMap.dcMotor.get("M_drive_BR");
+        this.M_pickup_TOP = hardwareMap.dcMotor.get("M_pickup_TOP");
+        this.M_pickup_BOTTOM = hardwareMap.dcMotor.get("M_pickup_BOTTOM");
         this.M_shooter = hardwareMap.dcMotor.get("M_shooter");
-        this.M_pickup = hardwareMap.dcMotor.get("M_pickup");
 
-        this.S_button_L = hardwareMap.servo.get("S_button_L");
-        this.S_button_R = hardwareMap.servo.get("S_button_R");
-
-        this.S_ballDrop = hardwareMap.servo.get("S_ballDrop");
+        this.S_button_L = hardwareMap.crservo.get("S_button_L");
+        this.S_button_R = hardwareMap.crservo.get("S_button_R");
+        this.S_ballDrop_TELEOP = hardwareMap.servo.get("S_ballDrop_TELEOP");
+        this.S_ballDrop_AUTON = hardwareMap.servo.get("S_ballDrop_AUTON");
 
         // fixing motor directions
         this.M_drive_L.setDirection(DcMotor.Direction.FORWARD);
         this.M_drive_R.setDirection(DcMotor.Direction.REVERSE);
-        
-        //initializing motor positions
-        this.M_shooter.setPosition(INIT_SHOOTER_POS);
 
         // initializing servo positions
-        this.S_button_L.setPosition(BUTTON_INIT_POS);
-        this.S_button_R.setPosition(BUTTON_INIT_POS);
-        this.S_ballDrop.setPosition(BALL_DROP_INIT);
+        this.S_button_L.setPower(BUTTON_INIT_STOP);
+        this.S_button_R.setPower(BUTTON_INIT_STOP);
 
         // wait for the game to start
         waitForStart();
@@ -98,8 +97,12 @@ public class SR_teleop extends LinearOpMode {
 
         while (opModeIsActive()) {
             // motor control block
-            M_drive_L.setPower(gamepad1.right_stick_y);
-            M_drive_R.setPower(gamepad1.left_stick_y);
+            M_drive_BL.setPower(gamepad1.right_stick_y);
+            M_drive_BR.setPower(gamepad1.left_stick_y);
+            M_drive_FL.setPower(gamepad1.right_stick_y);
+            M_drive_FR.setPower(gamepad1.left_stick_y);
+            M_lift_FL.setPower(gamepad2.right_stick_y);
+            M_lift_FR.setPower(gamepad2.left_stick_y);
 
             // shooter control block
             if(gamepad2.b){
@@ -131,7 +134,7 @@ public class SR_teleop extends LinearOpMode {
             // arm releases control block
             if(gamepad1.b){
                 ARM_POS_L += SERVO_TICK;
-                 S_liftSide_L.setPosition(ARM_POS_L);
+                S_liftSide_L.setPosition(ARM_POS_L);
                 ARM_POS_R -= SERVO_TICK;
                 S_liftSide_R.setPosition(ARM_POS_R);
             }
@@ -156,4 +159,5 @@ public class SR_teleop extends LinearOpMode {
         }
     }
 }
+
 
